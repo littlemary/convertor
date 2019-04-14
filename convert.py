a36e97261c1b2d43c60e8f18d3a4b18c1b838678
Mary <Mary>
2019-04-14 18:22:32 +0300

    Убрать в TeileNr=3 все елементы кроме Bnr=43

--- convert.py
+++ convert.py
@@ -127,26 +127,13 @@ def convertxml():
                     new_tag.attrib['WkzPos'] = '0'
 
                 if cur.attrib['TeileNr']=="2":
-                    mas2_el = cur.attrib
-                    for cur2_ in cur:
-                        if int(cur2_.attrib['BNr'])== 43:
-                            newtags2.append(cur2_.attrib)
-                    #update = {"cur1": mas2_el, "cur2":newtags2}
-                    child.remove(cur)
-
-                    new_tag = ET.SubElement(child, 'FensterTeiledaten')
-                    new_tag.attrib = mas2_el
-                    for newtags2_ in newtags2:
-                        new_tag1 = ET.SubElement(new_tag, 'FensterBearb')
-                        new_tag1.attrib = newtags2_
-
-                    newtags2.clear()
-                    #mas2.update(update)
-
-        #for child in c:
-         #   for cur in child:
-          #      if cur.attrib['TeileNr']=="2":
-           #         child.remove(cur)
+                    flag = False
+                    while flag==False:
+                        flag = True
+                        for cur2_ in cur:
+                            if int(cur2_.attrib['BNr'])!= 43:
+                                flag = False
+                                cur.remove(cur2_)
 
     strfile = xml.dom.minidom.parseString(
         ET.tostring(
